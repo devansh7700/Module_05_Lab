683ca2e8455e9dea4a61aedb4241ffbefd617046
Devanshgarg <dgarg3@rrc.ca>
2025-03-12 18:18:13 -0500

    Created a function that returns a promise with a setTimeout to simulate an asynchronous operation.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+function simulateAsyncOperation() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Async operation complete!");
+        }, 2000); // Simulate a 2-second delay
+    });
+}
+ 
+simulateAsyncOperation().then((message) => {
+    console.log(message);
+});
