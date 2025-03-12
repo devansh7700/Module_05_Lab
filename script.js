86bb55029752f52a441b575680d3b9b1cb7ec8df
Devanshgarg <dgarg3@rrc.ca>
2025-03-12 18:19:34 -0500

    Created an async function named fetchCatBreeds using fetch to retrieve data.

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ function simulateAsyncOperation() {
 simulateAsyncOperation().then((message) => {
     console.log(message);
 });
+
+async function fetchCatBreeds() {
+    try {
+        const response = await fetch('https://catfact.ninja/breeds');
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error fetching cat breeds:", error);
+    }
+}
+ 
+fetchCatBreeds().then((data) => console.log(data));
+ 
